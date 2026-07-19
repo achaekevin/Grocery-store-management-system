30503c8dd734b2bb272f6e46028ea2fba71fa1ad
achaekevin <achaekevin@gmail.com>
2026-07-19 21:13:18 +0300

    fix: model initialization

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -20,64 +20,65 @@ const testConnection = async () => {
 
 // Import models dynamically
 const initModels = async () => {
-  const Business = (await import('./Business.js')).default;
-  const Branch = (await import('./Branch.js')).default;
-  const Role = (await import('./Role.js')).default;
-  const Permission = (await import('./Permission.js')).default;
-  const RolePermission = (await import('./RolePermission.js')).default;
-  const User = (await import('./User.js')).default;
-  const Category = (await import('./Category.js')).default;
-  const Brand = (await import('./Brand.js')).default;
-  const Unit = (await import('./Unit.js')).default;
-  const Product = (await import('./Product.js')).default;
-  const Inventory = (await import('./Inventory.js')).default;
-  const InventoryMovement = (await import('./InventoryMovement.js')).default;
-  const Customer = (await import('./Customer.js')).default;
-  const Supplier = (await import('./Supplier.js')).default;
-  const Sale = (await import('./Sale.js')).default;
-  const SaleItem = (await import('./SaleItem.js')).default;
-  const Payment = (await import('./Payment.js')).default;
-  const PurchaseOrder = (await import('./PurchaseOrder.js')).default;
-  const PurchaseItem = (await import('./PurchaseItem.js')).default;
-  const Expense = (await import('./Expense.js')).default;
-  const ExpenseCategory = (await import('./ExpenseCategory.js')).default;
-  const MpesaTransaction = (await import('./MpesaTransaction.js')).default;
-  const LoyaltyTransaction = (await import('./LoyaltyTransaction.js')).default;
-  const AuditLog = (await import('./AuditLog.js')).default;
-  const Notification = (await import('./Notification.js')).default;
-  const Setting = (await import('./Setting.js')).default;
+  // Import all model initializer functions
+  const BusinessInit = (await import('./Business.js')).default;
+  const BranchInit = (await import('./Branch.js')).default;
+  const RoleInit = (await import('./Role.js')).default;
+  const PermissionInit = (await import('./Permission.js')).default;
+  const RolePermissionInit = (await import('./RolePermission.js')).default;
+  const UserInit = (await import('./User.js')).default;
+  const CategoryInit = (await import('./Category.js')).default;
+  const BrandInit = (await import('./Brand.js')).default;
+  const UnitInit = (await import('./Unit.js')).default;
+  const ProductInit = (await import('./Product.js')).default;
+  const InventoryInit = (await import('./Inventory.js')).default;
+  const InventoryMovementInit = (await import('./InventoryMovement.js')).default;
+  const CustomerInit = (await import('./Customer.js')).default;
+  const SupplierInit = (await import('./Supplier.js')).default;
+  const SaleInit = (await import('./Sale.js')).default;
+  const SaleItemInit = (await import('./SaleItem.js')).default;
+  const PaymentInit = (await import('./Payment.js')).default;
+  const PurchaseOrderInit = (await import('./PurchaseOrder.js')).default;
+  const PurchaseItemInit = (await import('./PurchaseItem.js')).default;
+  const ExpenseInit = (await import('./Expense.js')).default;
+  const ExpenseCategoryInit = (await import('./ExpenseCategory.js')).default;
+  const MpesaTransactionInit = (await import('./MpesaTransaction.js')).default;
+  const LoyaltyTransactionInit = (await import('./LoyaltyTransaction.js')).default;
+  const AuditLogInit = (await import('./AuditLog.js')).default;
+  const NotificationInit = (await import('./Notification.js')).default;
+  const SettingInit = (await import('./Setting.js')).default;
 
-  // Add models to db object
-  db.Business = Business;
-  db.Branch = Branch;
-  db.Role = Role;
-  db.Permission = Permission;
-  db.RolePermission = RolePermission;
-  db.User = User;
-  db.Category = Category;
-  db.Brand = Brand;
-  db.Unit = Unit;
-  db.Product = Product;
-  db.Inventory = Inventory;
-  db.InventoryMovement = InventoryMovement;
-  db.Customer = Customer;
-  db.Supplier = Supplier;
-  db.Sale = Sale;
-  db.SaleItem = SaleItem;
-  db.Payment = Payment;
-  db.PurchaseOrder = PurchaseOrder;
-  db.PurchaseItem = PurchaseItem;
-  db.Expense = Expense;
-  db.ExpenseCategory = ExpenseCategory;
-  db.MpesaTransaction = MpesaTransaction;
-  db.LoyaltyTransaction = LoyaltyTransaction;
-  db.AuditLog = AuditLog;
-  db.Notification = Notification;
-  db.Setting = Setting;
+  // Initialize all models by calling their init functions
+  db.Business = BusinessInit(sequelize);
+  db.Branch = BranchInit(sequelize);
+  db.Role = RoleInit(sequelize);
+  db.Permission = PermissionInit(sequelize);
+  db.RolePermission = RolePermissionInit(sequelize);
+  db.User = UserInit(sequelize);
+  db.Category = CategoryInit(sequelize);
+  db.Brand = BrandInit(sequelize);
+  db.Unit = UnitInit(sequelize);
+  db.Product = ProductInit(sequelize);
+  db.Inventory = InventoryInit(sequelize);
+  db.InventoryMovement = InventoryMovementInit(sequelize);
+  db.Customer = CustomerInit(sequelize);
+  db.Supplier = SupplierInit(sequelize);
+  db.Sale = SaleInit(sequelize);
+  db.SaleItem = SaleItemInit(sequelize);
+  db.Payment = PaymentInit(sequelize);
+  db.PurchaseOrder = PurchaseOrderInit(sequelize);
+  db.PurchaseItem = PurchaseItemInit(sequelize);
+  db.Expense = ExpenseInit(sequelize);
+  db.ExpenseCategory = ExpenseCategoryInit(sequelize);
+  db.MpesaTransaction = MpesaTransactionInit(sequelize);
+  db.LoyaltyTransaction = LoyaltyTransactionInit(sequelize);
+  db.AuditLog = AuditLogInit(sequelize);
+  db.Notification = NotificationInit(sequelize);
+  db.Setting = SettingInit(sequelize);
 
   // Define associations
   Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
+    if (db[modelName] && typeof db[modelName].associate === 'function') {
       db[modelName].associate(db);
     }
   });
