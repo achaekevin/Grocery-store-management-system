d190523477ce6518f4a8546275349a468cea2ee6
achaekevin <achaekevin@gmail.com>
2026-07-20 10:02:01 +0300

    fix: add @config path alias to vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       '@contexts': path.resolve(__dirname, './src/contexts'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@types': path.resolve(__dirname, './src/types'),
+      '@config': path.resolve(__dirname, './src/config'),
       '@assets': path.resolve(__dirname, './src/assets'),
     },
   },
